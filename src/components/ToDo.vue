c565d2024a54a8049c3a480c3fb4c206b5481481
ardianaiqbal <ardianaiqbal5@gmail.com>
2023-07-27 14:19:30 +0700

    commit1

---
+++ src/components/ToDo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ol>
+      <li v-bind:key="daftar" v-for="daftar in listDaftar">
+        {{ daftar }}
+      </li>
+    </ol>
+  </div>
+
+  <input v-model="tambahList" v-on:keyup.enter="tambahkan"/>
+  <button @click="tambahkan">tambahkan</button>
+  <p v-if="jumlah >= 4">Hebat!</p>
+  <div></div>
+</template>
+  
+  <script>
+export default {
+  name: "ToDo",
+  data() {
+    return {
+      listDaftar: [],
+      jumlah: 0,
+    };
+  },
+  methods: {
+    tambahkan() {
+        if(this.tambahList!=null&&this.tambahList!=''){
+            this.listDaftar.push(this.tambahList);
+      this.jumlah++;
+      this.tambahList = "";
+        }
+        else{
+            null;
+        }
+      
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
+  
